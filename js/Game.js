01dba7ef0c64ee61b4f75083ae0532bfd8ddb7d6
Matias Moraga <matimoraga10@gmail.com>
2023-11-03 20:03:56 -0300

    Agrego timer y imagenes al select

--- js/Game.js
+++ js/Game.js
@@ -5,23 +5,55 @@ class Game {
         this.disks = disks;
         this.players = players.slice(0, 2);//solo puedo tener 2 players
         this.currentTurn = 0; // Comienza con el primer jugador
-        
+
+        this.startingMinutes = 1;
+        this.time = this.startingMinutes * 60;
+        this.countDown = document.getElementById('timer');
+        this.timerInterval = null;
     }
 
-    winGame(){
+    startTimer() {
+        document.getElementById("timer").style.display = "block";
+        this.timerInterval = setInterval(() => {
+            this.updateTimer(); // Actualiza el temporizador cada segundo
+            if (this.time <= 0) {
+                clearInterval(this.timerInterval);
+                this.switchTurn(); // Cambia de jugador cuando el tiempo llega a 00
+                update();
+                this.time = this.startingMinutes * 60; // Reinicia el tiempo
+                this.startTimer(); // Inicia el temporizador nuevamente
+            }
+        }, 1000);
+    }
+
+    updateTimer() {
+        const minutes = Math.floor(this.time / 60);
+        const seconds = this.time % 60;
+        this.countDown.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        this.time--;
+    }
+
+    resetTimer() {
+        clearInterval(this.timerInterval); // Detiene el temporizador actual (si está en marcha)
+        this.time = this.startingMinutes * 60; // Reinicia el tiempo
+        this.startTimer(); // Inicia el temporizador nuevamente
+    }
+
+
+    winGame() {
         let cellsConnect = this.board.cellsConnect(this.connect);
-        if(cellsConnect.length > 0){
+        if (cellsConnect.length > 0) {
             this.showWinnerDisks(cellsConnect);
             this.switchTurn();
             console.log("Gano", this.getCurrentPlayer().getName());
             this.endGame();
             showButtonReset();
-            turn.textContent = 'GANO: ' +this.getCurrentPlayer().getName();
+            document.getElementById("winner").style.display = "block";
         }
     }
 
-    endGame(){
-        for(let i = 0; i < this.disks.length; i++){
+    endGame() {
+        for (let i = 0; i < this.disks.length; i++) {
             const disk = this.disks[i];
             disk.setMovido(true);
         }
@@ -35,6 +67,7 @@ class Game {
     }
 
     switchTurn() {
+        this.resetTimer();
         this.currentTurn = (this.currentTurn + 1) % this.players.length;
     }
 
--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 
 let btnMenu = document.querySelector(".menu-btn");
-btnMenu.addEventListener("click", function(){
-                                    let menuBuger = document.querySelector("#nav-containers");
-                                    menuBuger.classList.toggle("nav-container");
-                        });
+btnMenu.addEventListener("click", function () {
+  let menuBuger = document.querySelector("#nav-containers");
+  menuBuger.classList.toggle("nav-container");
+});
 
-var rotado = false;                      
-btnMenu.addEventListener("click", function(){
-  let logoMenu=document.querySelector(".menu-btn");
-  if(rotado){
-   
+var rotado = false;
+btnMenu.addEventListener("click", function () {
+  let logoMenu = document.querySelector(".menu-btn");
+  if (rotado) {
+
     logoMenu.style.transform = "rotate(0deg)";
-  }else{
+  } else {
     logoMenu.style.transform = "rotate(90deg)";
   }
 
@@ -19,47 +19,47 @@ btnMenu.addEventListener("click", function(){
 })
 
 let btnUser = document.querySelector(".user-btn");
-btnUser.addEventListener("click", function(){
-    let menuUser = document.querySelector("#user-containers");
-    menuUser.classList.toggle("user-container");
-    
+btnUser.addEventListener("click", function () {
+  let menuUser = document.querySelector("#user-containers");
+  menuUser.classList.toggle("user-container");
+
 });
 
 
 
-  var i = 0;
-  function move() {
-    if(window.location.href.includes("index.html")){
-      if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var width = 10;
-        var id = setInterval(frame, 50);
-        function frame() {
-          if (width >= 100) {
-            clearInterval(id);
-            i = 0;
-          } else {
-            width++;
-            elem.style.width = width + "%";
-            elem.innerHTML = width + "%";
-          }
+var i = 0;
+function move() {
+  if (window.location.href.includes("index.html")) {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 10;
+      var id = setInterval(frame, 50);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+          elem.innerHTML = width + "%";
         }
       }
     }
   }
+}
 
 move();
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Encuentra el div por su id
-  
+
   let load = document.getElementById("load-container");
-  if(window.location.href.includes("index.html")){
+  if (window.location.href.includes("index.html")) {
     // Establece un temporizador para ocultar el div después de 5 segundos (5000 milisegundos)
-    setTimeout(function() {
+    setTimeout(function () {
       load.style.display = "none"; // Oculta el div
     }, 5000); // 5000 milisegundos (5 segundos)
   }
@@ -70,12 +70,12 @@ const botonCompartir = document.getElementById("btn-share");
 const popup = document.getElementById("popup");
 const cerrarPopup = document.getElementById("cerrarPopup");
 
-if(window.location.href.includes("game.html")){
-  botonCompartir.addEventListener("click", function() {
+if (window.location.href.includes("game.html")) {
+  botonCompartir.addEventListener("click", function () {
     popup.style.display = "flex"; // Muestra el popup al hacer clic en el botón
   });
 
-  cerrarPopup.addEventListener("click", function() {
+  cerrarPopup.addEventListener("click", function () {
     popup.style.display = "none"; // Oculta el popup al hacer clic en el botón "Cerrar"
   });
 }
@@ -103,33 +103,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 //CODIGO PARA 4 en linea
-// function setupCustomDropdown(selectedOptionId, optionsListId) {
-//   const selectedOption = document.getElementById(selectedOptionId);
-//   const optionsList = document.getElementById(optionsListId);
-
-//   selectedOption.addEventListener("click", () => {
-//     optionsList.style.display = optionsList.style.display === "block" ? "none" : "block";
-//   });
-
-//   optionsList.addEventListener("click", (e) => {
-//     if (e.target.tagName === "LI") {
-//       const selectedValue = e.target.getAttribute("data-value");
-//       selectedOption.innerHTML = e.target.innerHTML;
-//       optionsList.style.display = "none";
-//       // Realiza acciones adicionales según la opción seleccionada si es necesario.
-//     }
-//   });
-// }
-
-//setupCustomDropdown("selectedOption", "optionsList");
-//setupCustomDropdown("selectedOption2", "optionsList2");
-
 
 document.getElementById("btn-play").addEventListener("click", function () {
 
   // Oculta el formulario
   document.getElementById("miFormulario").style.display = "none";
-  
+
   // Muestra el contenido adicional
   document.getElementById("turn").style.display = "block";
   document.getElementById("winner").style.display = "block";
@@ -148,6 +127,11 @@ const play = document.getElementById('btn-play');
 const formulario = document.getElementById('miFormulario');
 const playAgainButton = document.getElementById("play-again");
 
+const imgClub1Select = document.getElementById('img-club1');
+const imgClub2Select = document.getElementById('img-club2');
+const selectClub1 = document.getElementById('club1');
+const selectClub2 = document.getElementById('club2');
+
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
 let players = [];
@@ -164,171 +148,169 @@ let player1 = new Player("azul", disksA);
 let player2 = new Player("rojo", disksB);
 players.push(player1, player2);
 
-let game = new Game(players, board, figures,4);//capaz el 4 esta de mas
+let game = new Game(players, board, figures, 4);
 
 let lastClickedFigure = null;
 let isMouseDown = false;
 
-function createGame() {//TO DO: crear players, con nombres, crear array figures, disksA, disksB;
-    let cellSize = 90;
-    let valor = tipoJuego.value;
-    let rows = parseInt(valor) + 2;
-    let cols = parseInt(valor) + 3;
-    let boardWidth = cols * cellSize;
-    let boardHeight = rows * cellSize;
-    let startX = (canvasWidth - boardWidth) / 2;
-    //let startY = (canvasHeight - boardHeight) / 2;
-    let startY = 20;
-    board = new Board(rows, cols, cellSize, startX, startY);
-    game = new Game(players, board, figures,parseInt(valor));
-    addFigures();
-    board.draw(ctx);
-    CANT_FIG = board.getSize();
-    //update();
+function createGame() {
+  let cellSize = 90;
+  let valor = tipoJuego.value;
+  let rows = parseInt(valor) + 2;
+  let cols = parseInt(valor) + 3;
+  let boardWidth = cols * cellSize;
+  let boardHeight = rows * cellSize;
+  let startX = (canvasWidth - boardWidth) / 2;
+  let startY = 20;
+  board = new Board(rows, cols, cellSize, startX, startY);
+  game = new Game(players, board, figures, parseInt(valor));
+  addFigures();
+  board.draw(ctx);
+  CANT_FIG = board.getSize();
+  game.startTimer();
+  //update();
 }
+selectClub1.addEventListener('change', function () {
+  imgClub1Select.src = document.getElementById('club1').value + ".png";
+});
 
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
+selectClub2.addEventListener('change', function () {
+  imgClub2Select.src = document.getElementById('club2').value + ".png";
+});
 
-    club1 = document.getElementById('club1').value;
-    club2 = document.getElementById('club2').value;
 
-    player1.setName(club1);
-    player2.setName(club2);
+formulario.addEventListener('submit', function (event) {//asigno los clubes que eligio el jugador
+  event.preventDefault();
 
-    imgClub1 = new Image();
-    imgClub2 = new Image();
+  club1 = document.getElementById('club1').value;
+  club2 = document.getElementById('club2').value;
 
-    //diskRiver = new Image();
-    //diskRiver.src = 'river.png';
+  player1.setName(club1);
+  player2.setName(club2);
 
-    //diskBoca = new Image();
-    //diskBoca.src = 'boca.png';
+  imgClub1 = new Image();
+  imgClub2 = new Image();
 
-    createGame();
-
-    //setTimeout(() => {
-    //    addFigures();
-    //}, 333);
+  createGame();
 });
 
 let currentColor = 'blue';
-function addFigure() {
-    if (currentColor === 'blue') {
-        addCircle('blue');
-        currentColor = 'red';
-    } else {
-        addCircle('red');
-        currentColor = 'blue';
-    }
-    update();
+function addFigure() {//agrego las figuras de los 2 equipos
+  if (currentColor === 'blue') {
+    addCircle('blue');
+    currentColor = 'red';
+  } else {
+    addCircle('red');
+    currentColor = 'blue';
+  }
+  update();
 }
 
-function update(c) {
-    // Limpia el canvas
-    clearCanvas();
-    board.draw(ctx,c);
-    let player = game.getCurrentPlayer();
-    turn.textContent = 'Es el turno de:    ' + player.getName();
+function update(c) {//actualizo el canvas
+  // Limpia el canvas
+  clearCanvas();
+  board.draw(ctx, c);
+  let player = game.getCurrentPlayer();
+  turn.textContent = 'Es el turno de:    ' + player.getName();
 
-    for (let i = 0; i < figures.length; i++) {
-        figures[i].draw();
-    }
+  for (let i = 0; i < figures.length; i++) {
+    figures[i].draw();
+  }
 }
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function addCircle(team) {
+function addCircle(team) {//agrego el circle dependiendo el equipo
   let circleRadius = 30;
   let posX, posY;
   let boardYmax = canvasHeight - board.getStartY() - circleRadius - 10;
   let boardYmin = canvasHeight - board.getHeight() + board.getStartY();
   let numRandom = getRandomNumber(boardYmax, boardYmin);
   if (team === 'blue') {
-      var club = club1;
-      posX = Math.round(Math.random() * (board.getStartX() - circleRadius - circleRadius) + circleRadius);
-      posY = canvasHeight - circleRadius - numRandom;
-      let circle = new Circle(posX, posY, circleRadius, team, ctx, club1);
-      disksA.push(circle);
+    var club = club1;
+    posX = Math.round(Math.random() * (board.getStartX() - circleRadius - circleRadius) + circleRadius);//que aparezca en los containers
+    posY = canvasHeight - circleRadius - numRandom;
+    let circle = new Circle(posX, posY, circleRadius, team, ctx, club1);
+    disksA.push(circle);
   } else if (team === 'red') {
-      var club = club2;
-      const maxX = (canvasWidth - circleRadius) - board.getStartX() - board.getWidth() - circleRadius;
-      posX = Math.round(Math.random() * maxX + board.getStartX() + board.getWidth() + circleRadius);
-      posY = canvasHeight - circleRadius - numRandom;
-      let circle = new Circle(posX, posY, circleRadius, team, ctx, club2);
-      disksB.push(circle);
+    var club = club2;
+    const maxX = (canvasWidth - circleRadius) - board.getStartX() - board.getWidth() - circleRadius;
+    posX = Math.round(Math.random() * maxX + board.getStartX() + board.getWidth() + circleRadius);//que aparezca en los containers
+    posY = canvasHeight - circleRadius - numRandom;
+    let circle = new Circle(posX, posY, circleRadius, team, ctx, club2);
+    disksB.push(circle);
   }
 
-  let circle = new Circle(posX, posY, circleRadius, team, ctx,club);
+  let circle = new Circle(posX, posY, circleRadius, team, ctx, club);
   figures.push(circle);
 }
 
 function onMouseDown(e) {
-    isMouseDown = true;
+  isMouseDown = true;
 
-    if (lastClickedFigure != null && !lastClickedFigure.getMovido()) {
-        lastClickedFigure.setResaltado(false);
-        lastClickedFigure = null;
-    }
-    let player = game.getCurrentPlayer();
-    let clickFig = findClickedFigure(e.offsetX, e.offsetY);
-    if (clickFig != null && player.disksContains(clickFig)) {
-        clickFig.setResaltado(true);
-        lastClickedFigure = clickFig;
-    }
-    update();
+  if (lastClickedFigure != null && !lastClickedFigure.getMovido()) {
+    lastClickedFigure.setResaltado(false);
+    lastClickedFigure = null;
+  }
+  let player = game.getCurrentPlayer();
+  let clickFig = findClickedFigure(e.offsetX, e.offsetY);
+  if (clickFig != null && player.disksContains(clickFig)) {
+    clickFig.setResaltado(true);
+    lastClickedFigure = clickFig;
+  }
+  update();
 }
 
 function onMouseUp(e) {
-    isMouseDown = false;
-    disk = lastClickedFigure;
-    if(disk != null){
-        var col = board.getCol(disk);
-    }
+  isMouseDown = false;
+  disk = lastClickedFigure;
+  if (disk != null) {
+    var col = board.getCol(disk);
+  }
 
-    if (disk != null && col != null) {
-        let player = game.getCurrentPlayer();
+  if (disk != null && col != null) {
+    let player = game.getCurrentPlayer();
 
-        player.dropDisk(col, disk, board, game);
-        game.winGame();
-        //console.log(board.matrix);
-    } else if(disk != null && col === null) {
-        disk.posOriginal();
-        
-    }
-    update();
+    player.dropDisk(col, disk, board, game);
+    game.winGame();
+    //console.log(board.matrix);
+  } else if (disk != null && col === null) {
+    disk.posOriginal();
+
+  }
+  update();
 
 }
 
 function onMouseMove(e) {
-    if (isMouseDown && lastClickedFigure != null) {
-        lastClickedFigure.setPosition(e.offsetX, e.offsetY);
-        let col = board.getCol(lastClickedFigure);
-        update(col);
-    }
+  if (isMouseDown && lastClickedFigure != null) {
+    lastClickedFigure.setPosition(e.offsetX, e.offsetY);
+    let col = board.getCol(lastClickedFigure);
+    update(col);
+  }
 }
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 }
 
 
 function addFigures() {
-    addFigure();
-    if (figures.length < CANT_FIG) {
-        setTimeout(addFigures, 33);
-    }
+  addFigure();
+  if (figures.length < CANT_FIG) {
+    setTimeout(addFigures, 33);
+  }
 }
 
-playAgainButton.addEventListener('click', function(){
+playAgainButton.addEventListener('click', function () {
   resetGame();
   document.getElementById("canvas").style.display = "none";
   document.getElementById("turn").style.display = "none";
 });
 
-function showButtonReset(){
+function showButtonReset() {
 
   console.log("llegue");
   playAgainButton.style.display = "block";
@@ -340,7 +322,7 @@ function resetGame() {
   figures = [];
   disksA = [];
   disksB = [];
-  
+
   board = new Board(6, 7, 90, 485, 50); // Tablero por defecto
   CANT_FIG = board.getSize();
   board.draw(ctx);
@@ -362,7 +344,7 @@ function resetGame() {
   // Oculta el botón "Jugar de Nuevo"
   playAgainButton.style.display = "none";
   document.getElementById("miFormulario").style.display = "block";
-  
+
 }
 
 playAgainButton.addEventListener('click', resetGame);
@@ -372,12 +354,12 @@ function showButtonReset() {
 }
 
 function findClickedFigure(x, y) {
-    for (let i = 0; i < figures.length; i++) {
-        const element = figures[i];
-        if (element.isPointInside(x, y)) {
-            return element;
-        }
+  for (let i = 0; i < figures.length; i++) {
+    const element = figures[i];
+    if (element.isPointInside(x, y)) {
+      return element;
     }
+  }
 }
 
 canvas.addEventListener('mousedown', onMouseDown, false);
